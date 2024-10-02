a4b0e896fe82fc2b1067ca1c5a4dd8dcbb7bd85b
gatitos-presidenciales <yossijaki@hotmail.com>
2024-10-02 01:39:35 -0600

    Creacion de mi pagina de links

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { FaFacebook, FaYoutube, FaInstagram } from 'react-icons/fa';
+import profileImage from 'public/profile.png';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-h-screen flex flex-col justify-between bg-gray-100 p-4">
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl font-bold mb-4">Gatitos Presidenciales</h1>
+        <img
+          src={profileImage}
+          alt="Perfil"
+          className="w-32 h-32 rounded-full mb-4"
+        />
+        <h2 className="text-2xl mb-4">Sígueme en todas mis redes (:</h2>
+        <div className="space-y-4">
+          <a href="https://www.facebook.com/gatitos.presidenciales/" className="flex items-center justify-center text-center space-x-2 bg-blue-600 text-white px-4 py-2 rounded-full">
+            <FaFacebook />
+            <span>Facebook</span>
+          </a>
+          <a href="https://www.youtube.com/@GatitosPresidenciales" className="flex items-center justify-center text-center space-x-2 bg-red-600 text-white px-4 py-2 rounded-full">
+            <FaYoutube />
+            <span>YouTube</span>
+          </a>
+          <a href="https://instagram.com/GatitosPresidenciales" className="flex items-center justify-center text-center space-x-2 bg-pink-600 text-white px-4 py-2 rounded-full">
+            <FaInstagram />
+            <span>Instagram (En construcción)</span>
+          </a>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <footer className="text-gray-600 mt-8 flex items-center justify-center text-center">
+        Derechos reservados: Gatitos Presidenciales ®
+      </footer>
+    </div>
+  );
+};
 
-export default App
+export default App;
